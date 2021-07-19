aefc904b3e525f08fdeb892a2cbf400ce2695f8a
CristianGaribo <78895210+CristianGaribo@users.noreply.github.com>
2021-07-19 00:45:40 -0500

    Update Conetion.php
    cambio de provedor de BD

--- Conetion.php
+++ Conetion.php
@@ -1,10 +1,10 @@
 <?php
 
-    $servername = "DESKTOP-UQ7DEF8\SQLEXPRESS";
+    $servername = "bdwebredes.database.windows.net";
     $conexion = array(
-        "Database" => "Registro",
-        "UID" => "sa",
-        "PWD" => "123",
+        "Database" => "Formulario",
+        "UID" => "Lasport",
+        "PWD" => "Chabacano12",
         "CharacterSet" => "UTF-8"
     );
     $con = sqlsrv_connect($servername,$conexion);
